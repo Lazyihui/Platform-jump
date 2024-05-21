f24968a192cda957f78ccfee25d6f97e0114a186
包子 <chengbaozi6@qq.com>
2024-05-21 15:50:22 +0800

    <feature>add:RoleEntity

---
+++ Assets/Script_Runtime/GameBusiness/Entity/RoleEntity.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class RoleEntity : MonoBehaviour {
+    [SerializeField] Rigidbody2D rb;
+
+    [SerializeField] SpriteRenderer sr;
+
+    [SerializeField] Animator animator;
+
+
+    public void Ctor() { }
+
+
+}
--- Assets/Script_Runtime/MainClient.cs
+++ Assets/Script_Runtime/MainClient.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainClient : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Awake()
-    {
+public class MainClient : MonoBehaviour {
+    void Awake() {
         Debug.Log("MainClient Awake");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
     }
 }
